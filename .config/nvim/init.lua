5a085f3ed65ac8a1c82eee85e6e4d0f3deb8f6d2
Lollbrant <34800479+Lollbrant@users.noreply.github.com>
2025-07-08 22:12:19 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,11 +8,11 @@ vim.opt.clipboard = "unnamedplus" -- use system clipboard
 vim.opt.showmatch = true -- show the matching brackets, curly braces or parenteses
 
 vim.opt.linebreak = true -- enable line breaking
-vim.opt.wrap = true -- text wrapping
+vim.opt.wrap = true -- text wrapping 
 vim.opt.termguicolors = true -- enable 24-bit RGB color in the TUI
 
 vim.opt.incsearch = true            -- search as characters are entered
-vim.opt.hlsearch = true            -- do not highlight matches
+vim.opt.hlsearch = false            -- do not highlight matches
 vim.opt.ignorecase = true           -- ignore case in searches by default
 vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
 vim.opt.showbreak = "↪"
@@ -34,20 +34,14 @@ vim.opt.autoread = true
 vim.opt.cursorline = true
 vim.opt.autowrite = true
 
-vim.bo.textwidth = 90 -- maximum line length before wrapping the text
-
 vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
----- define your colorscheme here
---local colorscheme = 'monokai_pro'
---
---local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
---if not is_ok then
---    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
---    return
---end
 
 require("config.lazy")
+require("config/custom-commands")
+
+
+vim.cmd[[colorscheme tokyonight]]
