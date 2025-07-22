7f83ba97263b1202242bc1bae8a8fa5e621129d8
Lollbrant <34800479+Lollbrant@users.noreply.github.com>
2025-07-22 22:22:18 +0200

    Update nvim-treesitter.lua

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,12 @@ local M = {
     "nvim-treesitter/nvim-treesitter",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
+        configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "yaml", "toml", "markdown", "json", "bash", "php" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
     end,
 }
 
